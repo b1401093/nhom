c5e186007a2c7a01d8053584760765acd3ce4d2b
b1401093 <113407750+b1401093@users.noreply.github.com>
2022-10-25 17:05:01 +0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function test() {
-    document.getElementById("demo").innerHTML = "Chào bạn đến chơi !";
+
+function submit() {
+    let ten = document.getElementById("ten").value;
+    let email = document.getElementById("email").value;
+    document.getElementById("demo").innerHTML = "<br><br><br><h2>Chào "+ten+"!<br>email: "+email+"</h2><br><p>Cảm ơn bạn để lại lời nhắn!";
+    
 } 
