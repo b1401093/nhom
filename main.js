d8771d8e749d67a860492ece798b646969590b1b
b1401093 <113407750+b1401093@users.noreply.github.com>
2022-10-29 14:26:53 +0700

    update

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-
+// submit
 function submit() {
     let ten = document.getElementById("ten").value;
     let email = document.getElementById("email").value;
     document.getElementById("demo").innerHTML = "<br><br><br><h2>Chào "+ten+"!<br>email: "+email+"</h2><br><p>Cảm ơn bạn để lại lời nhắn!";
     
 } 
-
+// dk
+function dk(){
+    document.getElementById('dn').style.display='none';
+    document.getElementById('dk').style.display='block';
+}
 // Get the modal
 var modal = document.getElementById('dn');
-
+var modal2 = document.getElementById('dk');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
